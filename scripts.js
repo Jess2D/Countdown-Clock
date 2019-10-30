dd0c87d733b5f5b7ea2fa629c4df5398425a1069
jess2d <jessicap_silveira@hotmail.com>
2019-10-31 11:05:59 +1300

    Add event form minutes

--- scripts.js
+++ scripts.js
@@ -48,3 +48,10 @@ function startTimer(){
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer))
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+  });
