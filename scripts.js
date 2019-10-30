66078e3ea49f113ddf909e24517835c3999cda5e
jess2d <jessicap_silveira@hotmail.com>
2019-10-31 10:50:01 +1300

    Add Function displayEndTime()

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds){
     const now  = Date.now();
     const then = now + seconds*1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now())/1000);
 
@@ -25,4 +27,15 @@ function displayTimeLeft(seconds){
     const display = `${minutes}:${remainderSeconds < 10 ? '0': ''}${remainderSeconds}`;
     timerDisplay.textContent = display;
     document.title = display;
+    timerDisplay.textContent = display;
+}
+
+
+function displayEndTime(timestamp){
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be back at ${hour}:${minutes}`;
+
+
 }
